b2f0bbeb64c325798362808676a688f60e0d815d
Matheus Santos <matheus.ads.study@mail.com>
2021-03-14 14:32:45 -0300

    Projeto criado

---
+++ Iniciado-backend-do-app/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0a80eafa4f988f829767b5401fabddd4',
+    expiresIn: '1d',
+  },
+};
---
+++ Iniciado-backend-do-app/src/models/Appointment.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User) // Model pra onde aponta.
+  @JoinColumn({ name: 'provider_id' }) // Coluna de relacionamento.
+  provider: User; // Muitos Appointments(Agendamentos) para um usuários.
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
+
+export default Appointment;
